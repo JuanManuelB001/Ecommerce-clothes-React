1d76287cf3920ecb128dcbf74e5422b08dbbec30
Juan Manuel Bolivar Mojica <jbolivarm2@ucentral.edu.co>
2026-02-27 15:45:33 -0500

    navBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,16 @@
 import { Link } from "react-router-dom";
 import "../css/navBar.css"
 
-export function NavBar(){
-    return(
-        <div className="container">
-            navBar
-            <Link
-            to={'/add-cart'}
-            >
-            Cart 
-            </Link>
-        </div>
-    );
+export function NavBar() {
+  return (
+    <div className="container-nav">
+      <Link to="/" className="nav-button">
+        Inicio
+      </Link>
+
+      <Link to="/add-cart" className="nav-button">
+        Ver Carrito
+      </Link>
+    </div>
+  );
 }
