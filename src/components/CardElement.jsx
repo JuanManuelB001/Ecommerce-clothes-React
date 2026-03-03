5ba6d88cd5d0ffdce38b87334a908404cff90bdc
Juan Manuel Bolivar Mojica <jbolivarm2@ucentral.edu.co>
2026-03-03 14:41:44 -0500

    style cardElement.css

--- src/components/CardElement.jsx
+++ src/components/CardElement.jsx
@@ -1,14 +1,27 @@
 import { useCartStore } from "./useCartStore";
 import "../css/cardElement.css"
-
+import { Link, Links } from "react-router-dom";
+import { GetImage } from "./GetImage";
 export function CardElement(){
     const cart = useCartStore((state)=> state.cart)
-    console.log(cart)
+    //const deleteElement = useCartStore((state)=> state.removeItemCart())
+    const handleDelete = ()=>{
+        console.log("Producto Eliminado")
+    }
     return(
-        <div>
-           {cart.map((d)=>(
-            <div key={d.id} className="container-cart-product">
-                <p>{d.title}</p>
+        <div className="container-cart-product">
+           {cart.map((cloth)=>(
+            <div key={cloth.id} className="cart-product">
+               <Link
+               className="card-img"
+               to={`info-clothes/${cloth.id}`}
+               >
+               </Link>
+               <h3>{cloth.title}</h3>
+               <GetImage name={cloth.id} png={cloth.img}/>
+            <div className="button-cart">
+                <button onClick={handleDelete}>Delete</button>
+            </div>
             </div>
            ))}
             
