04bd4a32df9f42a0d1468a035a30baf5a550180f
Juan Manuel Bolivar Mojica <jbolivarm2@ucentral.edu.co>
2026-03-05 13:53:37 -0500

    change navBar.css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,13 +4,15 @@ import "../css/navBar.css"
 export function NavBar() {
   return (
     <div className="container-nav">
-      <Link to="/" className="nav-button">
+      <div className="nav-container">
+        <Link to="/" className="nav-button">
         Inicio
       </Link>
 
       <Link to="/add-cart" className="nav-button">
         Ver Carrito
       </Link>
+      </div>
     </div>
   );
 }
