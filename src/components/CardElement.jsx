2ba1b8baae18faebc2694a12086e7b6213435057
juan manuel <jbolivarm2@ucentral.edu.co>
2026-05-12 15:49:07 -0500

    create Swiper for Cart

--- src/components/CardElement.jsx
+++ src/components/CardElement.jsx
@@ -1,47 +1,68 @@
+import { useState } from "react";
 import { useCartStore } from "./useCartStore";
 import "../css/cardElement.css";
-import { Link, Links } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { GetImage } from "./GetImage";
 import { ConfirmDelete } from "./ConfirmDelete";
-import { use, useState } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
 export function CardElement() {
   const cart = useCartStore((state) => state.cart);
   const deleteElement = useCartStore((state) => state.removeItemCart);
-const [showConfirm, setShowConfirm] = useState(false)
-    const [clothId, setClothId] = useState();
-const handleConfirmDelete = ()=>{
+
+  const [showConfirm, setShowConfirm] = useState(false);
+  const [clothId, setClothId] = useState(null);
+
+  const handleConfirmDelete = () => {
     deleteElement(clothId);
     console.log(`Producto ${clothId} Eliminado`);
-    setShowConfirm(false)
-}
-const handleCancel = ()=>{
     setShowConfirm(false);
-    
-}
-const deleteClothId = (id) =>{
-    setClothId(id)
-    setShowConfirm(true)
+  };
 
-  }
+  const handleCancel = () => {
+    setShowConfirm(false);
+  };
+
+  const deleteClothId = (id) => {
+    setClothId(id);
+    setShowConfirm(true);
+  };
+
   return (
-    <div className="container-cart-product">
-      {cart.map((cloth) => (
-        <div key={cloth.id} className="cart-product">
-          <Link className="card-img" to={`info-clothes/${cloth.id}`}></Link>
-          <h3>{cloth.title}</h3>
-          <GetImage name={cloth.id} png={cloth.img} />
-          <div className="button-cart">
-            <button onClick={() => deleteClothId(cloth.id)}>Delete</button>
-          </div>
-        </div>
-      ))}
-      {
-        showConfirm && (<ConfirmDelete
-        message="¿Seguro que quieres vaciar el carrito?"
-        onConfirm={handleConfirmDelete}
-        onCancel={handleCancel}
-      />)
-      }
-    </div>
+    <>
+      <Swiper
+        className="netflix-swiper"
+        modules={[Navigation]}
+        navigation
+        slidesPerView={"auto"}
+        spaceBetween={20}
+        centeredSlides={false}
+        grabCursor={true}
+      >
+        {cart.map((item) => (
+          <SwiperSlide key={item.id} className="netflix-slide">
+            <div className="netflix-card">
+              <img src={item.img} alt={item.title} />
+              <h3>{item.title}</h3>
+            </div>
+
+            <div className="button-cart">
+              <button onClick={() => deleteClothId(item.id)}>Delete</button>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      {showConfirm && (
+        <ConfirmDelete
+          message="¿Seguro que quieres eliminar este producto?"
+          onConfirm={handleConfirmDelete}
+          onCancel={handleCancel}
+        />
+      )}
+    </>
   );
 }
